ead3109e4eba9215801d723a1832d5cff6ff8e96
xudoyberdi0410 <xudoyberdi0410@gmail.com>
2022-12-14 11:35:45 +0500

    tailwindcss

--- client/src/Auth/Auth.jsx
+++ client/src/Auth/Auth.jsx
@@ -17,6 +17,7 @@ const Auth = () => {
     }
     return ( 
         <main>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"></link>
             <div>
                 <input type="text" name="username" placeholder="Username" onChange={formHandler}/>
                 <input type="password" name="password" placeholder="Password" onChange={formHandler}/>
